559cf0d9233240187ad59ffa0d3251a0e9f50cd9
MashhoodYousafGhuman <mashhoodyousaf24@gmail.com>
2026-02-15 23:04:11 +0500

    edit .config.mjs for cloudinary images res from backend

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,27 @@
 /** @type {import('next').NextConfig} */
+
+const backendUrl =
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000";
+const url = new URL(backendUrl);
+
 const nextConfig = {
   images: {
     // domains: ["res.cloudinary.com", "unsplash.com", "cdn.example.com"], //  Cloudinary domain added
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "example.com",
+        hostname: "url.hostname",
         port: "", // optional, leave empty for default
         pathname: "/**", // allow all paths
       },
       {
         protocol: "https",
         hostname: "images.example.org",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com", // cloudinary 
         pathname: "/**",
       },
     ],
