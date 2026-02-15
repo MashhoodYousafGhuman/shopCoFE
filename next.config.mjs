23eb2d3a0b4a518ca76e783c2dcc75c4d1cfc9e8
MashhoodYousafGhuman <mashhoodyousaf24@gmail.com>
2026-02-15 21:55:29 +0500

    chainging for render

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com", "unsplash.com", "cdn.example.com"], //  Cloudinary domain added
+    // domains: ["res.cloudinary.com", "unsplash.com", "cdn.example.com"], //  Cloudinary domain added
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "example.com",
+        port: "", // optional, leave empty for default
+        pathname: "/**", // allow all paths
+      },
+      {
+        protocol: "https",
+        hostname: "images.example.org",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
